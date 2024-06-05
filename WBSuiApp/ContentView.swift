f36068e7bc34ba1bb6cfd4753a76c10aa809068f
Gleb Klyga <klyga90@bk.ru>
2024-06-05 10:18:01 +0300

    Initial Commit

---
+++ WBSuiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WBSuiApp
+//
+//  Created by Глеб Клыга on 5.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WBSuiApp/WBSuiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSuiAppApp.swift
+//  WBSuiApp
+//
+//  Created by Глеб Клыга on 5.06.24.
+//
+
+import SwiftUI
+
+@main
+struct WBSuiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
