28b9cad4e76483db0d51020adba0a1f44b1dd862
Gleb Klyga <klyga90@bk.ru>
2024-06-06 11:17:28 +0300

    Onboarding Screen

--- WBSuiApp/App/ContentView.swift
+++ WBSuiApp/App/ContentView.swift
@@ -10,10 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image("Illustration")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 300)
+            Spacer()
+            
+            Text("Общайтесь с друзьями\nи близкими легко")
+                .font(.sfProDisplay(size: 24, weight: .bold))
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer()
+            
+            Text("Пользовательское соглашение")
+                .font(.sfProDisplay(size: 14))
+                .foregroundColor(Color(hex: "#29183B"))
+                .padding(.bottom, 10)
+            
+            Button(action: {
+            }) {
+                Text("Начать общаться")
+                    .font(.sfProDisplay(size: 18, weight: .bold))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color(hex: "#9A41FE"))
+                    .cornerRadius(25)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 20)
         }
         .padding()
     }
